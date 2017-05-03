d0f805a9509fb0fcde7f2c886a64a6cf7441d6d6
sggb <shangggb@joyowo.com>
2017-05-03 10:42:52 +0800

    增加Hystrix

--- src/main/java/com/fishmeat/Application.java
+++ src/main/java/com/fishmeat/Application.java
@@ -2,6 +2,7 @@ package com.fishmeat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
