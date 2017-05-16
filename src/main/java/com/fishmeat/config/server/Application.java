42d70cf0b45d89e99652d6ba123d71f98e142bbe
sggb <shangggb@joyowo.com>
2017-05-16 13:25:08 +0800

    修改包路径

--- src/main/java/com/fishmeat/config/server/Application.java
+++ src/main/java/com/fishmeat/config/server/Application.java
@@ -1,4 +1,4 @@
-package com.fishmeat;
+package com.fishmeat.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
